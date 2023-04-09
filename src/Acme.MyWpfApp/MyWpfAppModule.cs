e3826f7d53beaf72be8bd70db9441cddac7c6021
黄毅 <huangyix@hnu.edu.cn>
2023-04-09 22:07:45 +0800

    Getting Started WPF Application

---
+++ src/Acme.MyWpfApp/MyWpfAppModule.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace Acme.MyWpfApp;
+
+[DependsOn(typeof(AbpAutofacModule))]
+public class MyWpfAppModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        context.Services.AddSingleton<MainWindow>();
+    }
+}
